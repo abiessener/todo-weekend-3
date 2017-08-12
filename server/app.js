9aa511084afa070189f323facb4501475eb0b760
Adam Biessener <abiessener@gmail.com>
2017-08-12 10:47:31 -0500

    created file structure, basic app.js functionality

---
+++ server/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var task = require('./routes/task')
+
+var app = express();
+
+var port = 5000;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use('/task', task);
+
+app.listen(port, function(){
+    console.log('listening on port', port);
+});
