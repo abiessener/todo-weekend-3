9bebc047a440615e1fb2a9ab94ec922546fa138e
Adam Biessener <abiessener@gmail.com>
2017-08-14 11:23:19 -0500

    trivial modification to initial SQL

--- dbcreate.sql
+++ dbcreate.sql
@@ -9,5 +9,5 @@ complete BOOLEAN
 INSERT INTO tasks (task, complete)
 VALUES ('sweep the floor', true),
 ('order pizza', false),
-('find the stupid flonase', false),
+('find the flonase', false),
 ('Dishes. Always and forever, dishes.', false)
