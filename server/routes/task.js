b6865400fd1a06abc30b1c2cddca3c459861f0af
Adam Biessener <abiessener@gmail.com>
2017-09-08 14:46:54 -0500

    cleaned up deprecated express calls

--- server/routes/task.js
+++ server/routes/task.js
@@ -12,13 +12,13 @@ router.get('/', function(req,res){
     pool.connect(function (errorConnectingToDB, client, done){
         if (errorConnectingToDB){
             console.log('error connecting to DB', errorConnectingToDB);
-            res.send(500);
+            res.sendStatus(500);
         } else {
             client.query('SELECT * FROM tasks ORDER BY id', function(errorMakingQuery, result){
                 done();
                 if (errorMakingQuery){
                     console.log('error making query', errorMakingQuery);
-                    res.send(500);
+                    res.sendStatus(500);
                 } else {
                     res.send(result.rows);
                 }
@@ -36,15 +36,15 @@ router.post('/', function(req,res){
     pool.connect(function (errorConnectingToDB, client, done){
         if (errorConnectingToDB){
             console.log('error connecting to DB', errorConnectingToDB);
-            res.send(500);
+            res.sendStatus(500);
         } else {
             client.query('INSERT INTO tasks (task, complete) VALUES ($1, $2)',[req.body.task, req.body.complete], function(errorMakingQuery, result){
                 done();
                 if (errorMakingQuery){
                     console.log('error making query', errorMakingQuery);
-                    res.send(500);
+                    res.sendStatus(500);
                 } else {
-                    res.send(201);
+                    res.sendStatus(201);
                 }
             })
         }
@@ -60,15 +60,15 @@ router.put('/:id', function(req,res){
     pool.connect(function (errorConnectingToDB, client, done){
         if (errorConnectingToDB){
             console.log('error connecting to DB', errorConnectingToDB);
-            res.send(500);
+            res.sendStatus(500);
         } else { // query like UPDATE tasks SET complete=true WHERE id=2;
             client.query('UPDATE tasks SET complete=$1 WHERE id=$2',[req.body.complete, req.params.id], function(errorMakingQuery, result){
                 done();
                 if (errorMakingQuery){
                     console.log('error making query', errorMakingQuery);
-                    res.send(500);
+                    res.sendStatus(500);
                 } else {
-                    res.send(200);
+                    res.sendStatus(200);
                 }
             })
         }
@@ -84,15 +84,15 @@ router.delete('/:id', function(req,res){
     pool.connect(function (errorConnectingToDB, client, done){
         if (errorConnectingToDB){
             console.log('error connecting to DB', errorConnectingToDB);
-            res.send(500);
+            res.sendStatus(500);
         } else { 
             client.query('DELETE FROM tasks WHERE id=$1',[req.params.id], function(errorMakingQuery, result){
                 done();
                 if (errorMakingQuery){
                     console.log('error making query', errorMakingQuery);
-                    res.send(500);
+                    res.sendStatus(500);
                 } else {
-                    res.send(200);
+                    res.sendStatus(200);
                 }
             })
         }
